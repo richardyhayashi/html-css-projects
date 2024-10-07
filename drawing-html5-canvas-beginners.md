9f1d9955d95a7f39ff2c0f1734a732292fe5bb7a
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-10-07 12:58:00 +0900

    Finished Chapter #2.

--- drawing-html5-canvas-beginners.md
+++ drawing-html5-canvas-beginners.md
@@ -6,7 +6,7 @@ YouTube: `https://www.youtube.com/playlist?list=PLpPnRKq7eNW3We9VdCfx9fprhqXHwTP
 Sources:
 
 1. Creating & Resizing the Canvas
-2.
+2. Drawing on the Canvas
 3.
 4.
 5.
--- src/canvas/js/canvas.js
+++ src/canvas/js/canvas.js
@@ -5,7 +5,39 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0, 0, 255, 0.5)';
 c.fillRect(200, 200, 100, 100);
+c.fillStyle = 'rgba(0, 255, 0, 0.5)';
 c.fillRect(300, 300, 100, 100);
 console.log(c);
+
+// Lines
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = '#fa34a3';
+c.stroke();
+
+// Arc/Circle
+// c.beginPath();
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = 'blue';
+// c.stroke();
+
+for (var i = 0; i < 1000; i++) {
+  let x = Math.random() * window.innerWidth;
+  let y = Math.random() * window.innerHeight;
+  let r = Math.floor(255 - Math.random() * 255);
+  let g = Math.floor(255 - Math.random() * 255);
+  let b = Math.floor(255 - Math.random() * 255);
+
+  //console.log(r, g, b);
+
+  c.beginPath();
+  c.arc(x, y, 30, 0, Math.PI * 2, false);
+  c.strokeStyle = `rgb(${r}, ${g}, ${b})`;
+  c.stroke();
+}
